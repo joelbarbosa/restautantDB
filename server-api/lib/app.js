fa2946876dccafd44014fb2d0026bb326679027f
joelbarbosa <joelbarbosa.ads@gmail.com>
2017-04-11 02:21:36 -0300

    config express routes es6

--- server-api/lib/app.js
+++ server-api/lib/app.js
@@ -1,10 +1,42 @@
 import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import Config from '../config/config.js';
+import fs from 'fs';
+import { Logger as log } from 'console4color';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ name: 'tobi' });
+log.info("port " + Config.port);
+
+app.set('port_server-api', Config.port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+fs.readdirSync('./lib/controllers').forEach(function(file) {  
+  if(file.substr(-3) == '.js') {
+  	const route = require('./controllers/' + file);
+  	route.controllers(app);
+  }
 });
 
-app.listen(3000, () => {
-  
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ name: 'tobi' });
+// });
+
+// app.listen(3000, () => {
+  
+// });
