4a694866a7c3fe2b9e1226d3ed0f8b6c4ee5ba85
Joel Ferreira Barbosa <u197676@marquise.com.br>
2017-04-12 18:05:09 -0300

    start frontend implementation

---
+++ front-api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'resources')));
+app.use("/resources",express.static("asset"));
+
+app.get('/', (req, res) => {  
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+app.listen(3001, () => {
+  console.log('Front app listening on port 3001');
+});
